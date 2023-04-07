a3ba38be8f74cb171f3871c27ad8dfcb48aa4077
LadyByrdie <eladden502@gmail.com>
2023-04-07 13:48:17 +0200

    au cas ou

--- src/model/ReservationRestaurant.java
+++ src/model/ReservationRestaurant.java
@@ -1,8 +1,21 @@
 package model;
 
-public class ReservationRestaurant {
-	private int jour;
-	private int mois;
-	private int service;
-	private int numeroTable;
+public class ReservationRestaurant extends Reservation{
+	int Service;
+	int numTable;
+	public ReservationRestaurant(int jour, int mois, int numService, int numTable) {
+		super(jour, mois);
+		this.Service = numService;
+		this.numTable = numTable;
+	}
+	public String toString() {
+		String chaine = "Le " + jour + "/" + mois + " : table n�" + numTable;
+		if(numService==1) {
+			chaine += " pour le premier service.";
+		}
+		else {
+			chaine += " pour le deuxième service.";
+		}
+		return chaine;
+	}
 }
